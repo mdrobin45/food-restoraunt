7e5fc0f0ca5b6dc55c56fc59d49501c42f132b2e
Md Robin Rana <robin4500bd@gmail.com>
2021-10-13 22:47:02 +0600

    first push code

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const API_KEY = process.env.REACT_APP_FIREBASE_PROJECT_API;
+console.log('The api key is: ', API_KEY)
+const App = () =>
+{
+  return (
+    <div>
+      <h2>Hello world</h2>
+    </div>
+  );
+};
+
+export default App;
